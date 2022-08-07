6d2acaa882bfe6a7f4023a3c2ae49156b0285d91
Slyusar Vladislav <flickervladislav@gmail.com>
2022-08-07 21:31:01 +0300

    feat(logger): add writing to a file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { readFile } from 'fs/promises';
 import { resolve } from 'path';
@@ -9,9 +9,13 @@ import * as yaml from 'js-yaml';
 import { LoggingService } from './modules/logging/logging.service';
 import { AppModule } from './app.module';
 
-const handleUncaughtErrors = () => {
-  const logger = new LoggingService();
+const initSwagger = async (app: INestApplication) => {
+  const openAPI = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
+  const document = yaml.load(openAPI);
+  SwaggerModule.setup('doc', app, document);
+};
 
+const handleUncaughtErrors = (logger: Logger) => {
   process
     .on('unhandledRejection', (reason) => {
       logger.error(`Unhandled rejection: ${reason}`);
@@ -22,29 +26,26 @@ const handleUncaughtErrors = () => {
     });
 };
 
-const initSwagger = async (app: INestApplication) => {
-  const openAPI = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
-  const document = yaml.load(openAPI);
-  SwaggerModule.setup('doc', app, document);
-};
-
-const initGlobalEntities = (app: INestApplication) => {
-  app.useLogger(new LoggingService());
+const initGlobalEntities = (app: INestApplication, logger: Logger) => {
+  app.useLogger(app.get(LoggingService));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  handleUncaughtErrors(logger);
 };
 
 const bootstrap = async () => {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 4000;
 
-  initGlobalEntities(app);
-  handleUncaughtErrors();
+  initGlobalEntities(app, logger);
 
   await initSwagger(app);
   await app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/modules/logging/logging.service.ts
+++ src/modules/logging/logging.service.ts
@@ -1,20 +1,87 @@
-import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
+import { ConsoleLogger, Injectable, LogLevel } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
 
-@Injectable({ scope: Scope.TRANSIENT })
+export const getLogLevels = (isProduction: boolean): LogLevel[] => {
+  if (isProduction) return ['log', 'warn', 'error'];
+  return ['error', 'warn', 'log', 'debug'];
+};
+
+@Injectable()
 export class LoggingService extends ConsoleLogger {
-  log(message: any, ...optionalParams: any[]) {
-    super.log(message, ...optionalParams);
+  private fileName = '';
+  private fileSize: number;
+
+  constructor(config: ConfigService) {
+    super();
+
+    this.setLogLevels(getLogLevels(config.get('NODE_ENV') === 'production'));
+    this.fileSize = Number(config.get('FILE_SIZE'));
   }
 
-  error(message: any, ...optionalParams: any[]) {
-    super.error(message, ...optionalParams);
+  log(message: string, context?: string) {
+    super.log.apply(this, [`\n${message}`, context]);
+    this.writeLogToFile(message, 'log', context);
   }
 
-  warn(message: any, ...optionalParams: any[]) {
-    super.warn(message, ...optionalParams);
+  error(message: string, context?: string) {
+    super.error.apply(this, [`\n${message}`, context]);
+    this.writeLogToFile(message, 'error', context);
   }
 
-  debug(message: any, ...optionalParams: any[]) {
-    super.debug(message, ...optionalParams);
+  warn(message: string, context?: string) {
+    super.warn.apply(this, [`\n${message}`, context]);
+    this.writeLogToFile(message, 'warn', context);
+  }
+
+  debug(message: string, context?: string) {
+    super.debug.apply(this, [`\n${message}`, context]);
+  }
+
+  private writeLogToFile = (
+    message: string,
+    type: string,
+    context?: string,
+  ): void => {
+    const amended = `\n${type.toUpperCase()} ${
+      context
+        ? '[' + context + ']'
+        : this.context
+        ? '[' + this.context + ']'
+        : ''
+    } ${message}`;
+
+    if (!this.fileName) {
+      this.createFileAndSave(amended, this.context);
+    } else {
+      this.amendFile(amended, this.context);
+    }
+  };
+
+  private createFileAndSave(message: string, context: string) {
+    if (context) {
+      this.fileName = `error_${context}_${Date.now()}.log`;
+      fs.appendFile(this.fileName, `${message}`, 'utf8', (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+
+  private amendFile(message: string, context: string) {
+    fs.stat(this.fileName, (err, stat) => {
+      if (err) {
+        const message = `Fail to open file ${this.fileName}`;
+        super.log.apply(this, [message, context]);
+      } else {
+        const fileSize = stat.size;
+
+        if (fileSize > this.fileSize) {
+          this.fileName = `error_${context}_${Date.now()}.log`;
+        }
+        fs.appendFile(this.fileName, `${message}`, 'utf8', (err) => {
+          if (err) throw err;
+        });
+      }
+    });
   }
 }
