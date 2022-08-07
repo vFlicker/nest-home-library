d7084b742c58f62ef18bf06233eac8a24a09b0a3
Slyusar Vladislav <flickervladislav@gmail.com>
2022-08-07 20:07:44 +0300

    feat: add handling of uncaught errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { readFile } from 'fs/promises';
 import { resolve } from 'path';
@@ -9,20 +9,45 @@ import * as yaml from 'js-yaml';
 import { LoggingService } from './modules/logging/logging.service';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+const handleUncaughtErrors = () => {
+  const logger = new LoggingService();
 
-  const configService = app.get(ConfigService);
+  process
+    .on('unhandledRejection', (reason) => {
+      logger.error(`Unhandled rejection: ${reason}`);
+    })
+    .on('uncaughtException', (error) => {
+      logger.error(`Uncaught exception: ${error}`);
+      process.exit(1);
+    });
+};
 
+const initSwagger = async (app: INestApplication) => {
   const openAPI = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
   const document = yaml.load(openAPI);
   SwaggerModule.setup('doc', app, document);
+};
 
+const initGlobalEntities = (app: INestApplication) => {
   app.useLogger(new LoggingService());
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
+};
 
-  await app.listen(configService.get('PORT') || 4000);
-}
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 4000;
+
+  initGlobalEntities(app);
+  handleUncaughtErrors();
+
+  await initSwagger(app);
+  await app.listen(port, () => console.log(`Listening on port ${port}`));
+};
+
 bootstrap();
